363b91782a397a9c7759c97c6b205d7a852cd842
vishvas <visuhadiyal@gmail.com>
2024-10-12 17:23:37 +0530

    static

--- qr_code/settings.py
+++ qr_code/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -37,6 +36,8 @@ CSRF_TRUSTED_ORIGINS = [
 
 # For production, set the STATIC_ROOT
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Directory to collect static files
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -129,15 +130,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
 import logging
@@ -148,7 +140,6 @@ from pathlib import Path
 
 # Other settings...
 
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 LOGGING = {
     'version': 1,
