d011ed2d50c1c6e689ef2ec40886fb7f0e7b142c
vishvas <visuhadiyal@gmail.com>
2024-10-12 15:31:35 +0530

    design

--- qr_code/settings.py
+++ qr_code/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -32,6 +32,10 @@ CSRF_TRUSTED_ORIGINS = [
     'https://9cf2-115-96-55-14.ngrok-free.app',
 ]
 # Application definition
+# Static files (CSS, JavaScript, Images)
+
+# For production, set the STATIC_ROOT
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
