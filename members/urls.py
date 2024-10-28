f6ebc11278682e0f0d2ecc9e3e45dbd43dad8a98
vishvas <visuhadiyal@gmail.com>
2024-10-28 21:53:10 +0530

    update redirect

--- members/urls.py
+++ members/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 from .views import cloudinary_config_view
+
+from django.views.generic import RedirectView
 urlpatterns = [
     # path('', views.members),
+     path('', RedirectView.as_view(url='/admin/', permanent=False)),
      path('cloudinary-config/', cloudinary_config_view, name='cloudinary_config'),
 ]
