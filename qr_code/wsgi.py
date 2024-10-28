6e03f5fd418a8ab31e973f2219391dc52ddb2843
vishvas <visuhadiyal@gmail.com>
2024-10-28 20:27:00 +0530

    solving static file issue

--- qr_code/wsgi.py
+++ qr_code/wsgi.py
@@ -2,12 +2,14 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from django.conf import settings
 from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qr_code.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
 
-application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'static'))
+# application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'static'))
 
 app = application # added for django vercel
